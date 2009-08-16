3f95475f0feee32e6e446ca6cfc38178c196dc7d
ulukyn <ulukyn@60ea55b9-4480-4529-b8ac-ed5291525b0f>
2009-08-16 18:41:51 +0000

    Fixed: #650 eventSetBotName works now

--- src/ai_service/ai_instance.cpp
+++ src/ai_service/ai_instance.cpp
@@ -854,6 +854,7 @@ void cbEventNpcGroupScript( NLNET::CMessage& msgin, const std::string &serviceNa
 	vector<string> strings;
 	msgin.serial(messageVersion);
 	nlassert(messageVersion==1);
+	msgin.serial(botId);
 	msgin.serial(nbString);
 	strings.resize(nbString);
 	for (uint32 i=0; i<nbString; ++i)
