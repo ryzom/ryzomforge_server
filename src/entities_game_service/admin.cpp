ee9228fcfb20ace5509245e02930a80db77727ab
ulukyn <ulukyn@60ea55b9-4480-4529-b8ac-ed5291525b0f>
2011-03-07 16:53:14 +0000

    Fixed: #1253 Change eventCreateNpcGroup command to specify position

--- src/entities_game_service/admin.cpp
+++ src/entities_game_service/admin.cpp
@@ -5407,7 +5407,7 @@ NLMISC_COMMAND(setFamePlayer, "set the fame value of a player in the given facti
 
 
 //----------------------------------------------------------------------------
-NLMISC_COMMAND(eventCreateNpcGroup, "create an event npc group", "<player eid> <nbBots> <sheet> [<dispersionRadius=10m> [<spawnBots=true> [<orientation=random|self|-360..360> [<name>]]]]")
+NLMISC_COMMAND(eventCreateNpcGroup, "create an event npc group", "<player eid> <nbBots> <sheet> [<dispersionRadius=10m> [<spawnBots=true> [<orientation=random|self|-360..360> [<name> [<x> [<y>]]]]]]")
 {
 	if (args.size () < 3) return false;
 	GET_ENTITY
@@ -5465,6 +5465,8 @@ NLMISC_COMMAND(eventCreateNpcGroup, "create an event npc group", "<player eid> <
 
 	std::string botsName;
 	if (args.size()>6) botsName = args[6];
+	if (args.size()>7) NLMISC::fromString(args[7], x);
+	if (args.size()>8) NLMISC::fromString(args[8], y);
 
 	CMessage msgout("EVENT_CREATE_NPC_GROUP");
 	uint32 messageVersion = 1;
