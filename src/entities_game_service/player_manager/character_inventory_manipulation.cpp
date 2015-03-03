2bf1b326b36e6bc53ccc2020cf6c76fb194fc255
ulukyn <devnull@localhost>
2015-03-03 12:16:21 +0100

    Changed: Fix forage tool required faction check

--- src/entities_game_service/player_manager/character_inventory_manipulation.cpp
+++ src/entities_game_service/player_manager/character_inventory_manipulation.cpp
@@ -1329,7 +1329,8 @@ bool CCharacter::checkPreRequired(const CGameItemPtr & item, bool equipCheck )
 	pair<PVP_CLAN::TPVPClan, PVP_CLAN::TPVPClan> allegeance = getAllegiance();
 	bool neutralcult = (allegeance.first == PVP_CLAN::Neutral || allegeance.first == PVP_CLAN::None);
 	bool neutralciv = (allegeance.second == PVP_CLAN::Neutral || allegeance.second == PVP_CLAN::None);
-	if ((item->getRequiredFaction() == "kami" && (allegeance.first != PVP_CLAN::Kami || getOrganization() != 0)) ||
+	if (item->getPhraseId().find("foragetool_") != 0 && (
+		(item->getRequiredFaction() == "kami" && (allegeance.first != PVP_CLAN::Kami || getOrganization() != 0)) ||
 		(item->getRequiredFaction() == "karavan" && (allegeance.first != PVP_CLAN::Karavan || getOrganization() != 0)) ||
 		(item->getRequiredFaction() == "marauder" && (!neutralcult || !neutralciv || getOrganization() != 5)) ||
 		(item->getRequiredFaction() == "neutralcult" && (!neutralcult || getOrganization() != 0)) ||
@@ -1338,7 +1339,7 @@ bool CCharacter::checkPreRequired(const CGameItemPtr & item, bool equipCheck )
 		(item->getRequiredFaction() == "fyros" && (allegeance.second != PVP_CLAN::Fyros || getOrganization() != 0)) ||
 		(item->getRequiredFaction() == "matis" && (allegeance.second != PVP_CLAN::Matis || getOrganization() != 0)) ||
 		(item->getRequiredFaction() == "tryker" && (allegeance.second != PVP_CLAN::Tryker || getOrganization() != 0)) ||
-		(item->getRequiredFaction() == "zorai" && (allegeance.second != PVP_CLAN::Zorai || getOrganization() != 0)))
+		(item->getRequiredFaction() == "zorai" && (allegeance.second != PVP_CLAN::Zorai || getOrganization() != 0))))
 			requiredRespected = false;
 	
 	if( requiredRespected == false && equipCheck )
