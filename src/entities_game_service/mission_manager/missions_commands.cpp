e5052596528fedb48a690d726b3b20e49f76e353
Riasan <riasan@ryzom.com>
2018-08-23 20:30:06 +0200

    Fixed: checkActionFlags using ark command works when dead.

--- src/entities_game_service/mission_manager/missions_commands.cpp
+++ src/entities_game_service/mission_manager/missions_commands.cpp
@@ -1953,10 +1953,14 @@ NLMISC_COMMAND(checkActionFlags,"Check Action Flags","<uid> [pvp_flag, pvp_tag, 
 		bypassCheckFlags.setFlag(CHECK_FLAG_TYPE::Invulnerability, args[1].length() > 7 && args[1][7] == '0');
 		bypassCheckFlags.setFlag(CHECK_FLAG_TYPE::Stun, args[1].length() > 8 && args[1][8] == '0');
 
-		if (!c->canEntityUseAction(bypassCheckFlags, true)) {
-			log.displayNL("ERR: OTHER_FLAG");
-			return false;
-		}
+		if (!c->canEntityUseAction(bypassCheckFlags, true))
+			{
+				if (!c->isDead() || (args[3].length() > 9 && args[3][9] == '1')) // Forbid if not dead or dead but not wanted
+				{
+				log.displayNL("ERR: OTHER_FLAG");
+				return false;
+				}
+			}
 	}
 	log.displayNL("OK");
 	return true;
