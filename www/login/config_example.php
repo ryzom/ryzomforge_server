1726bca6aba0877d6be74fa64219cfeded4533ff
ulukyn <devnull@localhost>
2019-03-26 16:34:07 +0100

    Fixed: rename config.php to config_example.php in www/login

---
+++ www/login/config_example.php
@@ -0,0 +1,22 @@
+<?php
+
+// This file contains all variables needed by other php scripts
+$LogRelativePath = 'logs/';
+
+// ----------------------------------------------------------------------------------------
+// Variables for nel database access
+// ----------------------------------------------------------------------------------------
+
+// where we can find the mysql database
+$DBHost         = 'localhost';
+$DBUserName     = 'USER';
+$DBPassword     = 'PASSWORD';
+$DBName         = 'nel';
+
+$RingDBUserName = 'RING_USER';
+$RingDBName = 'ring';
+$RingDBPassword = 'RING_PASS';
+
+$AcceptUnknownUser = true;
+$AutoCreateRingInfo = true;
+
