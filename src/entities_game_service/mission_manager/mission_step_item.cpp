d6dff4c32f1130f1dc7e2e8270a56e5d2da00675
ulukyn <devnull@localhost>
2019-06-25 21:56:10 +0200

    Added: Dirty way to add Craft & Give mission step (have less time to do it better...)

--- src/entities_game_service/mission_manager/mission_step_item.cpp
+++ src/entities_game_service/mission_manager/mission_step_item.cpp
@@ -19,19 +19,23 @@
 #include "stdpch.h"
 #include "nel/misc/algo.h"
 #include "mission_step_template.h"
+#include "game_item_manager/player_inv_xchg.h"
 #include "mission_manager/mission_template.h"
 #include "player_manager/character.h"
 #include "player_manager/player_manager.h"
+#include "creature_manager/creature_manager.h"
 #include "mission_log.h"
 #include "mission_manager/mission_parser.h"
 #include "mission_manager/ai_alias_translator.h"
 #include "egs_mirror.h"
 #include "primitives_parser.h"
 #include "egs_sheets/egs_sheets.h"
+#include "egs_pd.h"
 
 
 using namespace std;
 using namespace NLMISC;
+using namespace EGSPD;
 
 /***************************************************************************************************
 Steps linked with items
@@ -66,7 +70,7 @@ bool	IMissionStepItem::buildStep( uint32 line, const std::vector< std::string > 
 			if ((args.size() == 1) && (args[0] == "#dynamic#"))
 			{
 				subStep.Dynamic = missionData.Name;
-				subStep.Quantity = 1000; // Use a very high value to prevent end of mission
+				subStep.Quantity = 10000; // Use a very high value to prevent end of mission
 			}
 			////
 			else
@@ -130,6 +134,8 @@ inline void IMissionStepItem::getTextParams(uint & nbSubSteps, TVectorParamCheck
 	uint16 quality;
 	uint32 quantity;
 
+	bool isCraftAndGive = _CraftType == "Craft&Give";
+
 	for ( uint i  = 0; i < subStepStates.size();i++ )
 	{
 		if ( subStepStates[i] != 0 )
@@ -164,8 +170,6 @@ inline void IMissionStepItem::getTextParams(uint & nbSubSteps, TVectorParamCheck
 				if (params.size() > 3) {
 					NLMISC::fromString(params[3], quality);
 				}
-
-				nlinfo("Sheet : %s, Quality : %d, Quantity: %d", itemSheet.toString().c_str(), quality, quantity);
 				////
 			}
 
@@ -178,6 +182,9 @@ inline void IMissionStepItem::getTextParams(uint & nbSubSteps, TVectorParamCheck
 
 				retParams.push_back(STRING_MANAGER::TParam());
 				retParams.back().Type = STRING_MANAGER::integer;
+				if (isCraftAndGive)
+					retParams.back().Int = (uint)(quantity / 10000);
+				else
 				retParams.back().Int = quantity;
 
 				if (quality)
@@ -185,6 +192,13 @@ inline void IMissionStepItem::getTextParams(uint & nbSubSteps, TVectorParamCheck
 					retParams.push_back(STRING_MANAGER::TParam());
 					retParams.back().Type = STRING_MANAGER::integer;
 					retParams.back().Int = quality;
+				}
+
+				if (isCraftAndGive)
+				{
+					retParams.push_back(STRING_MANAGER::TParam());
+					retParams.back().Type = STRING_MANAGER::integer;
+					retParams.back().Int = quantity % 10000;
 				}
 			}
 		}
@@ -274,11 +288,8 @@ class CMissionStepLootItem : public IMissionStepItem
 		uint16 quantity = 1;
 		_User = PlayerManager.getChar(getEntityIdFromRow(userRow));
 
-		nlinfo("ok");
-		
 		if ( event.Type == CMissionEvent::LootItem )
 		{
-			nlinfo("ok");
 			CSheetId itemSheet;
 			uint16 quality;
 
@@ -316,7 +327,6 @@ class CMissionStepLootItem : public IMissionStepItem
 
 		
 			CMissionEventLootItem & eventSpe = (CMissionEventLootItem&)event;
-			nlinfo("Sheet : %s, Quality : %d / %d, Quantity: %d / %d", eventSpe.Sheet.toString().c_str(), eventSpe.Quality, quality, eventSpe.Quantity, quantity);
 			if ( eventSpe.Sheet == itemSheet && eventSpe.Quality >= quality )
 			{
 				if (!webAppUrl.empty())
@@ -400,7 +410,6 @@ uint CMissionStepLootRm::processEvent( const TDataSetRow & userRow, const CMissi
 		}
 		
 		CMissionEventLootRm & eventSpe = (CMissionEventLootRm&)event;
-		nlinfo("Sheet : %s, Quality : %d / %d, Quantity: %d / %d", eventSpe.Sheet.toString().c_str(), eventSpe.Quality, quality, eventSpe.Quantity, quantity);
 		if ( eventSpe.Sheet == itemSheet && eventSpe.Quality >= quality )
 		{
 			if (!webAppUrl.empty())
@@ -438,27 +447,252 @@ MISSION_REGISTER_STEP(CMissionStepLootRm,"loot_mp")
 // ----------------------------------------------------------------------------
 class CMissionStepCraft : public IMissionStepItem
 {
-	uint processEvent( const TDataSetRow & userRow, const CMissionEvent & event,uint subStepIndex,const TDataSetRow & giverRow )
+	virtual bool	buildStep( uint32 line, const std::vector< std::string > & script, CMissionGlobalParsingData & globalData, CMissionSpecificParsingData & missionData )
 	{
-		if ( event.Type == CMissionEvent::Craft )
+		_SourceLine = line;
+		_HasBot = false;
+		_Bot = CAIAliasTranslator::Invalid;
+		if ( !IMissionStepItem::buildStep(line, script,globalData,missionData) )
+			return false;
+		if ( script.size() == 3 )
 		{
-			CMissionEventCraft & eventSpe = (CMissionEventCraft&)event;
-			if ( eventSpe.Sheet == _SubSteps[subStepIndex].Sheet &&
-				eventSpe.Quality >= _SubSteps[subStepIndex].Quality )
+			_HasBot = true;
+			if ( !CMissionParser::parseBotName(script[2],_Bot,missionData) )
+				return false;
+		}
+		return true;
+	}
+
+	virtual uint32 sendContextText(const TDataSetRow& user, const TDataSetRow& interlocutor, CMission * instance, bool & gift, const NLMISC::CEntityId & giver )
+	{
+		if (_HasBot) {
+			CCreature * bot = CreatureManager.getCreature( interlocutor );
+			if ( bot )
 			{
-				LOGMISSIONSTEPSUCCESS("craft");
-				return eventSpe.Quantity;
+				if ( ( _Bot != CAIAliasTranslator::Invalid && _Bot == bot->getAlias() ) ||
+					( _Bot == CAIAliasTranslator::Invalid && bot->getAlias() == instance->getGiver() ) )
+				{
+					gift = true;
+					TVectorParamCheck vect(1);
+					vect[0].Type = STRING_MANAGER::integer;
+					vect[0].Int = (sint32)_SubSteps.size();
+
+					CMissionParser::solveEntitiesNames(vect,user,giver);
+					return STRING_MANAGER::sendStringToClient( user, "MIS_GIVE_ITEM_MENU" , vect );
+				}
+			}
+			else
+			{
+				MISLOG("sline:%u ERROR : give_item (sendContextText) : invalid bot %u", _SourceLine, interlocutor.getIndex());
 			}
 		}
 		return 0;
 	}
 
+	virtual bool hasBotChatOption(const TDataSetRow & interlocutor, CMission * instance, bool & gift)
+	{
+		if (!_HasBot)
+			return false;
+
+		CCreature * bot = CreatureManager.getCreature( interlocutor );
+		if ( ( _Bot != CAIAliasTranslator::Invalid && _Bot == bot->getAlias() ) ||
+			( _Bot == CAIAliasTranslator::Invalid && bot->getAlias() == instance->getGiver() ) )
+		{
+			gift = true;
+			return true;
+		}
+		else
+		{
+			MISLOG("sline:%u ERROR : craft (hasBotChatOption) : invalid bot %u", _SourceLine, interlocutor.getIndex());
+		}
+		return false;
+	}
+
+	uint processEvent( const TDataSetRow & userRow, const CMissionEvent & event,uint subStepIndex,const TDataSetRow & giverRow )
+	{
+		if ( event.Type == CMissionEvent::Craft )
+		{
+			CMissionEventCraft & eventSpe = (CMissionEventCraft&)event;
+			if ( eventSpe.Sheet == _SubSteps[subStepIndex].Sheet && eventSpe.Quality >= _SubSteps[subStepIndex].Quality )
+			{
+				LOGMISSIONSTEPSUCCESS("craft");
+				if ( _HasBot )
+					return eventSpe.Quantity*10000;
+				return eventSpe.Quantity;
+			}
+		}
+		else if( event.Type == CMissionEvent::GiveItem ) {
+			if ( _HasBot )
+			{
+				const CMissionEventGiveItem& eventSpe = (const CMissionEventGiveItem&)event;
+				if ( subStepIndex == eventSpe.StepIndex )
+				{
+					LOGMISSIONSTEPSUCCESS("give_item");
+					return eventSpe.Quantity % 10000;
+				}
+			}
+		}
+		return 0;
+	}
+
+	void getInitState( std::vector<uint32>& ret )
+	{
+		ret.clear();
+		ret.resize( _SubSteps.size() );
+		for ( uint i = 0; i < _SubSteps.size(); i++ )
+		{
+			// Dirty hack by Ulukyn to manage craft and give item in same step (The state are : Craft + 1000*Give)
+			ret[i] = _SubSteps[i].Quantity+10000*_SubSteps[i].Quantity;
+		}
+	}
+
+
 	virtual void getTextParams( uint & nbSubSteps, const std::string* & textPtr,TVectorParamCheck& retParams, const std::vector<uint32>& subStepStates)
 	{
+		if ( _HasBot )
+			_CraftType = "Craft&Give";
+
 		IMissionStepItem::getTextParams(nbSubSteps, retParams, subStepStates);
 		static const std::string stepText = "MIS_CRAFT_";
+		static const std::string stepTextNpc = "MIS_CRAFT_NPC_";
+		if ( _HasBot )
+		{
+			STRING_MANAGER::TParam param;
+			param.Type = STRING_MANAGER::bot;
+
+			if ( _Bot != CAIAliasTranslator::Invalid )
+				param.Int = _Bot;
+			else
+				param.Identifier = "giver";
+			retParams.push_back(param);
+
+			textPtr = &stepTextNpc;
+		}
+		else
 		textPtr = &stepText;
 	}
+
+	virtual TAIAlias getInvolvedBot(bool& invalidIsGiver) const { invalidIsGiver=true; return _Bot; }
+
+	//***********************
+	bool checkPlayerGift( CMission * instance, CCharacter * user )
+	{
+		if ( user->getExchangeMoney() != 0 )
+			return false;
+
+		std::vector<CSubStep> validateSteps = _SubSteps;
+
+		// build the list of need item by substracting the template step with the current step state
+		// TODO : this code is only aware of the first active step !
+		if (instance->getStepsBegin() == instance->getStepsEnd())
+			return false;
+
+		const CActiveStepPD &activeStep = instance->getStepsBegin()->second;
+
+		for (uint i=0; i<_SubSteps.size(); ++i)
+		{
+			const CActiveStepStatePD *activeStepState = activeStep.getStates(i+1);
+			if (activeStepState != NULL) {
+				validateSteps[i].Quantity = activeStepState->getState() % 10000;
+			}
+		}
+		bool notEmpty = false;
+
+		nlassert(user->getExchangeView() != NULL);
+		for (uint i = 0; i < CExchangeView::NbExchangeSlots; i++)
+		{
+			uint32 exchangeQuantity;
+			CGameItemPtr item = user->getExchangeView()->getExchangeItem(i, &exchangeQuantity);
+			if (item == NULL)
+				continue;
+
+			notEmpty = true;
+			uint k = 0;
+			for (; k < validateSteps.size(); k++)
+			{
+				if (item->getSheetId() == validateSteps[k].Sheet && item->recommended() >= validateSteps[k].Quality)
+				{
+					if (exchangeQuantity > validateSteps[k].Quantity)
+					{
+						// if user has put more that this sub step quantity, just decrease the quantity
+						exchangeQuantity -= validateSteps[k].Quantity;
+						validateSteps[k].Quantity = 0;
+					}
+					else
+					{
+						// This sub step consume all this stack in the exchange inventory
+						validateSteps[k].Quantity -= exchangeQuantity;
+						exchangeQuantity = 0;
+					}
+				}
+			}
+			// the give is invalid only if there are still some quantity in the exchange bag
+			if (exchangeQuantity > 0)
+				return false;
+		}
+		return notEmpty;
+	}
+
+	//***********************
+	bool itemGiftDone( CCharacter & user , const std::vector< CGameItemPtr > & itemsGiven, const EGSPD::CActiveStepPD & step, std::vector<uint32>& result )
+	{
+		// init the result vector with the initial values of the sub steps
+		for ( map<uint32,EGSPD::CActiveStepStatePD>::const_iterator it = step.getStatesBegin(); it != step.getStatesEnd(); ++it )
+		{
+			result.push_back( (*it).second.getState() % 10000 );
+		}
+		// iterate through the exchange inventory items. For now Store in result the remianing quantity of each substep
+		for ( uint i  =0; i < itemsGiven.size(); i++ )
+		{
+			if ( itemsGiven[i] != NULL)
+			{
+
+
+				// the slot is not empty : get quantity / quality /sheet. Special case if we have a stack...
+				CSheetId sheet = itemsGiven[i]->getSheetId();
+				uint32 quantity = itemsGiven[i]->getStackSize();
+				uint32 recommended = itemsGiven[i]->recommended();
+
+				// check if a step matches the item? ( we are sure that the steps are sorted by quality, so the first step who matches will ve the good one )
+				bool noMatch = true;
+				for ( uint k = 0; k < _SubSteps.size(); k++ )
+				{
+					if ( _SubSteps[k].Sheet == sheet && _SubSteps[k].Quality <= recommended )
+					{
+						noMatch = false;
+						if ( quantity > result[k] )
+						{
+							quantity -= result[k];
+							result[k] = 0;
+							if ( quantity == 0)
+								break;
+						}
+						else
+						{
+							result[k] -= quantity;
+							quantity = 0;
+							break;
+						}
+					}
+				}
+				if( noMatch )
+					return false;
+			}
+		}
+
+		// we now store in result the actual given quantity
+		uint i = 0;
+		for ( map<uint32,EGSPD::CActiveStepStatePD>::const_iterator it = step.getStatesBegin(); it != step.getStatesEnd(); ++it )
+		{
+			result[i] = (*it).second.getState() - result[i];
+			i++;
+		}
+		return true;
+	}
+
+
+	bool _HasBot;
+	TAIAlias _Bot;
 
 	MISSION_STEP_GETNEWPTR(CMissionStepCraft)
 };
